c62deec4438555091e21defe9f254ee847875c05
Paul Kuruvilla <rohitpaulk@gmail.com>
2018-12-13 01:25:29 +0530

    Add command to run executable

---
+++ README.md
@@ -0,0 +1,11 @@
+# Redis Challenge Tester
+
+This is a program that validates your progress on the Redis challenge.
+
+Usage:
+
+```
+./tester --stage 1 --binary ./subject_bin
+```
+
+
--- main.go
+++ main.go
@@ -1,8 +1,47 @@
 package main
 
+import "flag"
 import "fmt"
+import "os"
+import "os/exec"
+import "time"
 
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("Welcome to the redis challenge!")
+	fmt.Println("")
+	binaryPathPtr := flag.String("binary-path", "", "path to the redis executable to test. Ex: ./run_redis.sh")
+
+	flag.Parse()
+	if *binaryPathPtr == "" {
+		fmt.Println("The --binary-path flag must be specified")
+		os.Exit(1)
+	}
+	fmt.Println("Binary Path =", *binaryPathPtr)
+	fmt.Println("")
+
+	cmd, err := runBinary(*binaryPathPtr)
+	if err != nil {
+		fmt.Printf("Error when starting process: %s", err)
+		fmt.Println("")
+		os.Exit(1)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	if cmd.Process != nil {
+		cmd.Process.Kill()
+	}
+	fmt.Println("Waiting for process to exit", *binaryPathPtr)
+	cmd.Wait()
+	fmt.Println("Tests done")
 }
 
+func runBinary(binaryPath string) (*exec.Cmd, error) {
+	command := exec.Command(binaryPath)
+	err := command.Start()
+	if err != nil {
+		return nil, err
+	}
+
+	return command, nil
+}
