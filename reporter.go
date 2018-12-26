454a94a2a2c1af751ba4fd8fc583efd0953e1980
Paul Kuruvilla <rohitpaulk@gmail.com>
2018-12-26 14:38:15 +0530

    More structure in reporter.go

--- reporter.go
+++ reporter.go
@@ -1,6 +1,13 @@
 package main
 
-func report(result StageRunnerResult) {
+func report(result StageRunnerResult) error {
 	logger := getLogger(false, "[reporter] ")
 	logger.Infoln("Submitting test results...")
+
+	err := tellLeaderboard("abcd", result.lastStageIndex)
+	return err
+}
+
+func tellLeaderboard(apiKey string, stage int) error {
+	return nil
 }
