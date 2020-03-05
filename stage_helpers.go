62bc49073a3ea5ea3b1c52328a31a6c4ff2313ad
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-03-05 17:21:20 +0530

    Wait for a whole second, not just 500ms

--- stage_helpers.go
+++ stage_helpers.go
@@ -24,7 +24,7 @@ func (b *RedisBinary) Run() error {
 		return err
 	}
 
-	time.Sleep(500 * time.Millisecond)
+	time.Sleep(1000 * time.Millisecond)
 
 	// ctx := context.Background()
 	// ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
