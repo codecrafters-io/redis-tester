412374dd5a1cead049a207b2b814eb41f50862ee
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-03-05 13:46:33 +0530

    Add more stages to Redis challenge

--- test_ping_pong.go
+++ test_ping_pong.go
@@ -7,7 +7,7 @@ import (
 	"github.com/go-redis/redis"
 )
 
-func testPingPong(executable *Executable, logger *customLogger) error {
+func testPingPongOnce(executable *Executable, logger *customLogger) error {
 	b := NewRedisBinary(executable, logger)
 	if err := b.Run(); err != nil {
 		return err
@@ -28,8 +28,97 @@ func testPingPong(executable *Executable, logger *customLogger) error {
 		return fmt.Errorf("Expected PONG, got %s", pong)
 	}
 
-	logger.Debugf("Success, closing connection with client...")
+	logger.Debugf("Success, closing connection...")
 	client.Close()
+
+	return nil
+}
+
+func testPingPongMultiple(executable *Executable, logger *customLogger) error {
+	b := NewRedisBinary(executable, logger)
+	if err := b.Run(); err != nil {
+		return err
+	}
+	defer b.Kill()
+
+	for i := 1; i <= 3; i++ {
+		client := redis.NewClient(&redis.Options{
+			Addr:        "localhost:6379",
+			DialTimeout: 30 * time.Second,
+		})
+		if err := runPing(logger, client, i); err != nil {
+			return err
+		}
+
+		logger.Debugf("client-%d: Success, closing connection...", i)
+		client.Close()
+	}
+
+	return nil
+}
+
+func testPingPongConcurrent(executable *Executable, logger *customLogger) error {
+	b := NewRedisBinary(executable, logger)
+	if err := b.Run(); err != nil {
+		return err
+	}
+	defer b.Kill()
+
+	client1 := redis.NewClient(&redis.Options{
+		Addr:        "localhost:6379",
+		DialTimeout: 30 * time.Second,
+	})
+	if err := runPing(logger, client1, 1); err != nil {
+		return err
+	}
+
+	client2 := redis.NewClient(&redis.Options{
+		Addr:        "localhost:6379",
+		DialTimeout: 30 * time.Second,
+	})
+	if err := runPing(logger, client2, 2); err != nil {
+		return err
+	}
+
+	if err := runPing(logger, client1, 1); err != nil {
+		return err
+	}
+	if err := runPing(logger, client1, 1); err != nil {
+		return err
+	}
+	if err := runPing(logger, client2, 2); err != nil {
+		return err
+	}
+
+	logger.Debugf("client-%d: Success, closing connection...", 1)
+	client1.Close()
+
+	client3 := redis.NewClient(&redis.Options{
+		Addr:        "localhost:6379",
+		DialTimeout: 30 * time.Second,
+	})
+	if err := runPing(logger, client3, 3); err != nil {
+		return err
+	}
+
+	logger.Debugf("client-%d: Success, closing connection...", 2)
+	client2.Close()
+	logger.Debugf("client-%d: Success, closing connection...", 3)
+	client3.Close()
+
+	return nil
+}
+
+func runPing(logger *customLogger, client *redis.Client, clientNum int) error {
+	logger.Debugf("client-%d: Sending ping command...", clientNum)
+	pong, err := client.Ping().Result()
+	if err != nil {
+		return err
+	}
+
+	if pong != "PONG" {
+		return fmt.Errorf("client-%d: Expected PONG, got %s", clientNum, pong)
+	}
 
 	return nil
 }
