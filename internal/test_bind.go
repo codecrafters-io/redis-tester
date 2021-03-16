3b437f4a381a3e1b7cbbe875c6643c81dd0b40d7
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-03-16 10:37:26 +0530

    Tweak timeout

--- internal/test_bind.go
+++ internal/test_bind.go
@@ -20,7 +20,7 @@ func testBindToPort(stageHarness testerutils.StageHarness) error {
 	var err error
 	for {
 		_, err = net.Dial("tcp", "localhost:6379")
-		if err != nil && retries > 5 {
+		if err != nil && retries > 20 {
 			logger.Debugf("All retries failed.")
 			return err
 		}
