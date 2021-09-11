799829e519b1502691457d28311427f9b6c70268
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-09-11 01:51:22 +0100

    update eof test

--- internal/test_ping_pong.go
+++ internal/test_ping_pong.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	testerutils "github.com/codecrafters-io/tester-utils"
@@ -27,6 +28,10 @@ func testPingPongOnce(stageHarness *testerutils.StageHarness) error {
 			logger.Infof("Hint: EOF is short for 'end of file'. This usually means that your program either:")
 			logger.Infof(" (a) didn't send a complete response, or")
 			logger.Infof(" (b) closed the connection early")
+		}
+
+		if strings.Contains(err.Error(), "connection reset by peer") {
+			logger.Infof("Hint: 'connection reset by peer' usually means that your program closed the connection before sending a complete response.")
 		}
 
 		return fmt.Errorf("failed to read response, err: %s", err)
