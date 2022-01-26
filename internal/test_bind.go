de19c94e0486a24ede9b02f5112ff29a7c722e09
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-01-26 22:15:30 +0000

    Revert "try a 30-second timeout"
    This reverts commit 429817c6169409aa61b0373094818ea5270c6b8b.

--- internal/test_bind.go
+++ internal/test_bind.go
@@ -19,7 +19,7 @@ func testBindToPort(stageHarness *testerutils.StageHarness) error {
 	var err error
 	for {
 		_, err = net.Dial("tcp", "localhost:6379")
-		if err != nil && retries > 60 {
+		if err != nil && retries > 20 {
 			logger.Infof("All retries failed.")
 			return err
 		}
--- internal/tester_definition.go
+++ internal/tester_definition.go
@@ -2,61 +2,59 @@ package internal
 
 import (
 	testerutils "github.com/codecrafters-io/tester-utils"
-	"time"
 )
 
 var testerDefinition = testerutils.TesterDefinition{
 	AntiCheatStages: []testerutils.Stage{
 		{
-			Slug:                    "anti-cheat-1",
-			Title:                   "Anti-cheat 1",
-			TestFunc:                antiCheatTest,
+			Slug:     "anti-cheat-1",
+			Title:    "Anti-cheat 1",
+			TestFunc: antiCheatTest,
 			ShouldRunPreviousStages: true,
 		},
 	},
 	ExecutableFileName: "spawn_redis_server.sh",
 	Stages: []testerutils.Stage{
 		{
-			Slug:                    "init",
-			Title:                   "Bind to a port",
-			TestFunc:                testBindToPort,
-			ShouldRunPreviousStages: true,
-			Timeout:                 30 * time.Second,
-		},
-		{
-			Slug:                    "ping-pong",
-			Title:                   "Respond to PING",
-			TestFunc:                testPingPongOnce,
+			Slug:     "init",
+			Title:    "Bind to a port",
+			TestFunc: testBindToPort,
 			ShouldRunPreviousStages: true,
 		},
 		{
-			Slug:                    "ping-pong-multiple",
-			Title:                   "Respond to multiple PINGs",
-			TestFunc:                testPingPongMultiple,
+			Slug:     "ping-pong",
+			Title:    "Respond to PING",
+			TestFunc: testPingPongOnce,
 			ShouldRunPreviousStages: true,
 		},
 		{
-			Slug:                    "concurrent-clients",
-			Title:                   "Handle concurrent clients",
-			TestFunc:                testPingPongConcurrent,
+			Slug:     "ping-pong-multiple",
+			Title:    "Respond to multiple PINGs",
+			TestFunc: testPingPongMultiple,
 			ShouldRunPreviousStages: true,
 		},
 		{
-			Slug:                    "echo",
-			Title:                   "Implement the ECHO command",
-			TestFunc:                testEcho,
+			Slug:     "concurrent-clients",
+			Title:    "Handle concurrent clients",
+			TestFunc: testPingPongConcurrent,
 			ShouldRunPreviousStages: true,
 		},
 		{
-			Slug:                    "set_get",
-			Title:                   "SET & GET",
-			TestFunc:                testGetSet,
+			Slug:     "echo",
+			Title:    "Implement the ECHO command",
+			TestFunc: testEcho,
 			ShouldRunPreviousStages: true,
 		},
 		{
-			Slug:                    "expiry",
-			Title:                   "Expiry!",
-			TestFunc:                testExpiry,
+			Slug:     "set_get",
+			Title:    "SET & GET",
+			TestFunc: testGetSet,
+			ShouldRunPreviousStages: true,
+		},
+		{
+			Slug:     "expiry",
+			Title:    "Expiry!",
+			TestFunc: testExpiry,
 			ShouldRunPreviousStages: true,
 		},
 	},
