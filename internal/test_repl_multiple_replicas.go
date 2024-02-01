8301df3e6215e8da4384578de9fe72cee268bf42
Ryan Gang <ryan-gg@outlook.com>
2024-02-01 17:36:53 +0000

    Add stubs for final 2 stages.

---
+++ internal/test_repl_multiple_replicas.go
@@ -0,0 +1,40 @@
+package internal
+
+import (
+	"fmt"
+
+	testerutils "github.com/codecrafters-io/tester-utils"
+)
+
+func testReplMultipleReplicas(stageHarness *testerutils.StageHarness) error {
+	master := NewRedisBinary(stageHarness)
+	master.args = []string{
+		"--port", "6379",
+	}
+
+	if err := master.Run(); err != nil {
+		return err
+	}
+
+	replica := NewRedisBinary(stageHarness)
+	replica.args = []string{
+		"--port", "6379",
+		"--replicaof", "localhost", "6380",
+	}
+
+	if err := replica.Run(); err != nil {
+		return err
+	}
+
+	logger := stageHarness.Logger
+	logger.Infof("Test : 13")
+
+	addr := "localhost:6379"
+	masterClient := NewRedisClient(addr)
+	addr = "localhost:6380"
+	replicaClient := NewRedisClient(addr)
+
+	masterClient.Close()
+	replicaClient.Close()
+	return fmt.Errorf("Test not implemented.")
+}
