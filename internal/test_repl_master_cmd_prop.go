54056a51dee56c6145bea76dc6211c4d61903d21
Ryan Gang <ryan-gg@outlook.com>
2024-02-02 10:10:03 +0000

    Add proper resp parsing while reading RDB. Refactor code, add logging.

--- internal/test_repl_master_cmd_prop.go
+++ internal/test_repl_master_cmd_prop.go
@@ -3,17 +3,14 @@ package internal
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"net"
 	"strings"
-	"time"
 
 	testerutils "github.com/codecrafters-io/tester-utils"
-	"github.com/hdt3213/rdb/parser"
 	"github.com/smallnest/resp3"
 )
 
 func testReplMasterCmdProp(stageHarness *testerutils.StageHarness) error {
+	deleteRDBfile()
 	master := NewRedisBinary(stageHarness)
 	master.args = []string{
 		"--port", "6379",
@@ -39,60 +36,45 @@ func testReplMasterCmdProp(stageHarness *testerutils.StageHarness) error {
 	logger.Infof("$ redis-cli PING")
 
 	w.WriteCommand("PING")
-	res, _, _ := r.ReadValue()
-	message := res.SmartResult()
-	respStr, _ := message.(string)
-	respParts := strings.Split(respStr, " ")
+	actualMessage, err := readRespString(r, logger)
+	if err != nil {
+		return err
+	}
+	if actualMessage != "PONG" {
+		return fmt.Errorf("Expected 'PONG', got %v", actualMessage)
+	}
 	logger.Successf("PONG received.")
 
 	logger.Infof("$ redis-cli REPLCONF listening-port 6380")
-
 	w.WriteCommand("REPLCONF", "listening-port", "6380")
-	res, _, _ = r.ReadValue()
-	message = res.SmartResult()
-	respStr, _ = message.(string)
-	respParts = strings.Split(respStr, " ")
+	actualMessage, err = readRespString(r, logger)
+	if err != nil {
+		return err
+	}
+	if actualMessage != "OK" {
+		return fmt.Errorf("Expected 'OK', got %v", actualMessage)
+	}
 	logger.Successf("OK received.")
 
 	w.WriteCommand("PSYNC", "?", "-1")
-	res, _, _ = r.ReadValue()
-	message = res.SmartResult()
-	respStr, _ = message.(string)
-	respParts = strings.Split(respStr, " ")
-	command := respParts[0]
-	offset := respParts[2]
-
+	actualMessage, err = readRespString(r, logger)
+	if err != nil {
+		return err
+	}
+	actualMessageParts := strings.Split(actualMessage, " ")
+	command, offset := actualMessageParts[0], actualMessageParts[2]
 	if command != "FULLRESYNC" {
-		return fmt.Errorf("Expected FULLRESYNC from Master, received %v", command)
+		return fmt.Errorf("Expected 'FULLRESYNC' from Master, got %v", command)
 	}
 	logger.Successf("FULLRESYNC received.")
 	if offset != "0" {
-		return fmt.Errorf("Expected offset to be 0 from Master, received %v", offset)
+		return fmt.Errorf("Expected Offset to be 0 from Master, got %v", offset)
 	}
-	logger.Successf("offset = 0 received.")
+	logger.Successf("Offset = 0 received.")
 
 	reader := bufio.NewReader(conn)
-	var data []byte
-	timeout := 2 * time.Second
-	conn.SetReadDeadline(time.Now().Add(timeout))
 
-	for {
-		b, err := reader.ReadByte()
-		if err != nil {
-			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-				break
-			}
-		}
-		data = append(data, b)
-	}
-
-	conn.SetReadDeadline(time.Time{})
-
-	dataString := string(data)[6:]
-	// First 6 chars are for RESP `$176\r\n` or similar.
-	stringIOReader := strings.NewReader(dataString)
-	decoder := parser.NewDecoder(stringIOReader)
-	err = decoder.Parse(processRedisObject)
+	err = readAndCheckRDBFile(reader)
 	if err != nil {
 		return fmt.Errorf("Error while parsing RDB file : %v", err)
 	}
@@ -101,66 +83,36 @@ func testReplMasterCmdProp(stageHarness *testerutils.StageHarness) error {
 	key1, value1 := "foo", "123"
 	key2, value2 := "bar", "456"
 	key3, value3 := "baz", "789"
+	setMap := map[int][]string{
+		1: {key1, value1},
+		2: {key2, value2},
+		3: {key3, value3},
+	}
+	for i := 1; i <= len(setMap); i++ { // We need order of commands preserved
+		key, value := setMap[i][0], setMap[i][1]
+		logger.Debugf("Setting key %s to %s", key, value)
+		client.Do("SET", key, value)
+	}
 
-	logger.Debugf("Setting key %s to %s", key1, value1)
-	client.Do("SET", key1, value1)
-	logger.Debugf("Setting key %s to %s", key2, value2)
-	client.Do("SET", key2, value2)
-	logger.Debugf("Setting key %s to %s", key3, value3)
-	client.Do("SET", key3, value3)
-
-	var cmds [][]string
-	conn.SetReadDeadline(time.Now().Add(timeout))
-
-	for {
-		req, err := Decode(reader)
-		if err != nil {
-			if err == io.EOF {
-				continue
-			}
-			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-				break
-			}
-			fmt.Println(err)
-			break
-		}
-
-		if len(req.Array()) == 0 {
-			continue
-		}
-
+	i := 0
+	for i < 3 {
+		req := parseRESPCommand(reader, false)
 		var cmd []string
 		for _, v := range req.Array() {
 			cmd = append(cmd, v.String())
 		}
-		cmds = append(cmds, cmd)
+		if len(cmd) > 0 && strings.ToUpper(cmd[0]) == "SET" {
+			// User might not send SELECT, but Redis will send SELECT
+			// Apart from SELECT we need 3 SETs
+			i += 1
+			key, value := setMap[i][0], setMap[i][1]
+			err := compareStringSlices(cmd, []string{"SET", key, value})
+			if err != nil {
+				return err
+			}
+			logger.Successf("Received %v", strings.Join(cmd, " "))
+		}
 	}
-
-	index := 0
-	possibleSelect := cmds[index]
-	if strings.ToUpper(possibleSelect[0]) == "SELECT" {
-		index += 1
-	}
-
-	err = (compareStringSlices(cmds[index], []string{"SET", key1, value1}))
-	if err != nil {
-		return err
-	}
-	logger.Successf("Received %v", strings.Join(cmds[index], " "))
-	index += 1
-
-	err = (compareStringSlices(cmds[index], []string{"SET", key2, value2}))
-	if err != nil {
-		return err
-	}
-	logger.Successf("Received %v", strings.Join(cmds[index], " "))
-	index += 1
-
-	err = (compareStringSlices(cmds[index], []string{"SET", key3, value3}))
-	if err != nil {
-		return err
-	}
-	logger.Successf("Received %v", strings.Join(cmds[index], " "))
 
 	conn.Close()
 	return nil
--- internal/test_repl_master_psync_rdb.go
+++ internal/test_repl_master_psync_rdb.go
@@ -3,16 +3,14 @@ package internal
 import (
 	"bufio"
 	"fmt"
-	"net"
 	"strings"
-	"time"
 
 	testerutils "github.com/codecrafters-io/tester-utils"
-	"github.com/hdt3213/rdb/parser"
 	"github.com/smallnest/resp3"
 )
 
 func testReplMasterPsyncRdb(stageHarness *testerutils.StageHarness) error {
+	deleteRDBfile()
 	master := NewRedisBinary(stageHarness)
 	master.args = []string{
 		"--port", "6379",
@@ -24,85 +22,60 @@ func testReplMasterPsyncRdb(stageHarness *testerutils.StageHarness) error {
 
 	logger := stageHarness.Logger
 
-	client := NewRedisClient("localhost:6379")
+	conn, err := NewRedisConn("", "localhost:6379")
 
-	logger.Infof("$ redis-cli PING")
-	resp, err := client.Do("PING").Result()
-
-	if err != nil {
-		logFriendlyError(logger, err)
-		return err
-	}
-
-	if resp != "PONG" {
-		return fmt.Errorf("Expected OK from Master, received %v", resp)
-	}
-	logger.Successf("PONG received.")
-
-	logger.Infof("$ redis-cli REPLCONF listening-port 6380")
-	resp, err = client.Do("REPLCONF", "listening-port", "6380").Result()
-
-	if err != nil {
-		logFriendlyError(logger, err)
-		return err
-	}
-
-	if resp != "OK" {
-		return fmt.Errorf("Expected OK from Master, received %v", resp)
-	}
-	logger.Successf("OK received.")
-
-	conn, err := net.Dial("tcp", ":6379")
 	if err != nil {
 		fmt.Println("Error connecting to TCP server:", err)
 	}
-	defer conn.Close()
 
 	r := resp3.NewReader(conn)
 	w := resp3.NewWriter(conn)
 
-	w.WriteCommand("PSYNC", "?", "-1")
-	response, _, _ := r.ReadValue()
-	message := response.SmartResult()
-	respStr, _ := message.(string)
-	respParts := strings.Split(respStr, " ")
-	command := respParts[0]
-	offset := respParts[2]
+	logger.Infof("$ redis-cli PING")
+	w.WriteCommand("PING")
+	actualMessage, err := readRespString(r, logger)
+	if err != nil {
+		return err
+	}
+	if actualMessage != "PONG" {
+		return fmt.Errorf("Expected 'PONG', got %v", actualMessage)
+	}
+	logger.Successf("PONG received.")
 
+	logger.Infof("$ redis-cli REPLCONF listening-port 6380")
+	w.WriteCommand("REPLCONF", "listening-port", "6380")
+	actualMessage, err = readRespString(r, logger)
+	if err != nil {
+		return err
+	}
+	if actualMessage != "OK" {
+		return fmt.Errorf("Expected 'OK', got %v", actualMessage)
+	}
+	logger.Successf("OK received.")
+
+	w.WriteCommand("PSYNC", "?", "-1")
+	actualMessage, err = readRespString(r, logger)
+	if err != nil {
+		return err
+	}
+	actualMessageParts := strings.Split(actualMessage, " ")
+	command, offset := actualMessageParts[0], actualMessageParts[2]
 	if command != "FULLRESYNC" {
-		return fmt.Errorf("Expected FULLRESYNC from Master, received %v", command)
+		return fmt.Errorf("Expected 'FULLRESYNC' from Master, got %v", command)
 	}
 	logger.Successf("FULLRESYNC received.")
 	if offset != "0" {
-		return fmt.Errorf("Expected offset to be 0 from Master, received %v", offset)
+		return fmt.Errorf("Expected Offset to be 0 from Master, got %v", offset)
 	}
-	logger.Successf("offset = 0 received.")
+	logger.Successf("Offset = 0 received.")
 
 	reader := bufio.NewReader(conn)
-	var data []byte
-	timeout := 5 * time.Second
 
-	for {
-		conn.SetReadDeadline(time.Now().Add(timeout))
-
-		b, err := reader.ReadByte()
-		if err != nil {
-			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-				break
-			}
-		}
-		data = append(data, b)
-	}
-
-	dataString := string(data)[6:]
-	// First 6 chars are for RESP `$176\r\n` or similar.
-	stringIOReader := strings.NewReader(dataString)
-	decoder := parser.NewDecoder(stringIOReader)
-	err = decoder.Parse(processRedisObject)
+	err = readAndCheckRDBFile(reader)
 	if err != nil {
 		return fmt.Errorf("Error while parsing RDB file : %v", err)
 	}
 	logger.Successf("RDB file received from master.")
-	client.Close()
+
 	return nil
 }
