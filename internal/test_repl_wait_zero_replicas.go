613af72cf6ef8c76cfe32d553c256b419fe313a6
Ryan Gang <ryan-gg@outlook.com>
2024-02-09 10:04:39 +0000

    Add Repl-16 test.

---
+++ internal/test_repl_wait_zero_replicas.go
@@ -0,0 +1,35 @@
+package internal
+
+import (
+	"fmt"
+
+	testerutils "github.com/codecrafters-io/tester-utils"
+)
+
+func testWaitZeroReplicas(stageHarness *testerutils.StageHarness) error {
+	deleteRDBfile()
+	master := NewRedisBinary(stageHarness)
+	master.args = []string{
+		"--port", "6379",
+	}
+
+	if err := master.Run(); err != nil {
+		return err
+	}
+
+	logger := stageHarness.Logger
+
+	conn, err := NewRedisConn("", "localhost:6379")
+	if err != nil {
+		fmt.Println("Error connecting to TCP server:", err)
+	}
+
+	client := NewFakeRedisMaster(conn, logger)
+
+	err = client.Wait("0", "60000", 0)
+	if err != nil {
+		return err
+	}
+	conn.Close()
+	return nil
+}
