3707e2fa428c89c20256b1dc6aeb977bbffe0915
Ryan Gang <ryan-gg@outlook.com>
2024-02-11 16:48:47 +0000

    Instead of timeout, start goroutine to accept connections and perform handshake in the bg.

--- internal/test_repl_info_replica.go
+++ internal/test_repl_info_replica.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"net"
 	"strings"
-	"time"
 
 	testerutils "github.com/codecrafters-io/tester-utils"
+	loggerutils "github.com/codecrafters-io/tester-utils/logger"
 )
 
 func testReplInfoReplica(stageHarness *testerutils.StageHarness) error {
@@ -29,20 +29,22 @@ func testReplInfoReplica(stageHarness *testerutils.StageHarness) error {
 		return err
 	}
 
-	timeout := 2 * time.Second
-	err = listener.(*net.TCPListener).SetDeadline(time.Now().Add(timeout))
-	if err != nil {
-		fmt.Println("Error setting deadline:", err.Error())
-		return err
-	}
-
-	conn, err := listener.Accept()
-	if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+	go func(l net.Listener) error {
 		// Connecting to master in this stage is optional.
-	} else if err != nil {
-		fmt.Println("Error accepting: ", err.Error())
-		return err
-	}
+		conn, err := listener.Accept()
+		if err != nil {
+			logger.Debugf("Error accepting: %s", err.Error())
+			return err
+		}
+
+		quietLogger := loggerutils.GetQuietLogger("")
+		master := NewFakeRedisMaster(conn, quietLogger)
+		master.Handshake()
+
+		conn.Close()
+		return nil
+	}(listener)
+
 	client := NewRedisClient("localhost:6380")
 
 	logger.Infof("$ redis-cli INFO replication")
@@ -66,6 +68,5 @@ func testReplInfoReplica(stageHarness *testerutils.StageHarness) error {
 	}
 
 	client.Close()
-	conn.Close()
 	return nil
 }
