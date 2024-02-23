0d878530ccd4659e5e8d5fd35a99a8e618465c66
libmartinito <lib.martinito@pm.me>
2024-02-23 15:24:29 +0800

    CC-986 updated tester definition

--- internal/tester_definition.go
+++ internal/tester_definition.go
@@ -148,5 +148,9 @@ var testerDefinition = testerutils.TesterDefinition{
 			Slug:     "streams-xadd",
 			TestFunc: testStreamsXadd,
 		},
+		{
+			Slug:     "streams-xadd-partial-autoid",
+			TestFunc: testStreamsXaddPartialAutoid,
+		},
 	},
 }
