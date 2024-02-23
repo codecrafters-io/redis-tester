b3e2f5eb02ebf9186f5ec6a0749b48c924922850
libmartinito <lib.martinito@pm.me>
2024-02-24 00:05:12 +0800

    CC-986 added tester definition

--- internal/tester_definition.go
+++ internal/tester_definition.go
@@ -148,5 +148,9 @@ var testerDefinition = testerutils.TesterDefinition{
 			Slug:     "streams-xadd",
 			TestFunc: testStreamsXadd,
 		},
+		{
+			Slug:     "streams-xrange",
+			TestFunc: testStreamsXrange,
+		},
 	},
 }
