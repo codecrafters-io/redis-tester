140e723a308dec8b31dc16bdb51ce2346ee21086
libmartinito <lib.martinito@pm.me>
2024-02-27 01:36:42 +0800

    CC-986 updated tester function + updated fixtures

--- internal/test_streams_xread_block.go
+++ internal/test_streams_xread_block.go
@@ -1,7 +1,10 @@
 package internal
 
 import (
+	"fmt"
 	"math/rand"
+	"reflect"
+	"time"
 
 	testerutils "github.com/codecrafters-io/tester-utils"
 	"github.com/go-redis/redis"
@@ -39,13 +42,64 @@ func testStreamsXreadBlock(stageHarness *testerutils.StageHarness) error {
 		expectedResponse: "0-1",
 	})
 
-	logger.Infof("$ redis-cli xread block 1000 streams %s 0-0", randomKey)
+	go func() {
+		time.Sleep(500 * time.Millisecond)
 
-	resp, _ := client.XRead(&redis.XReadArgs{
-		Streams: []string{randomKey, "0-0"},
-		Block:   1000,
+		testXadd(client, logger, XADDTest{
+			streamKey:        randomKey,
+			id:               "0-2",
+			values:           map[string]interface{}{"bar": "baz"},
+			expectedResponse: "0-2",
+		})
+	}()
+
+	logger.Infof("$ redis-cli xread block 1000 streams %s 0-1", randomKey)
+
+	resp, err := client.XRead(&redis.XReadArgs{
+		Block:   1000 * time.Millisecond,
+		Streams: []string{randomKey, "0-1"},
 	}).Result()
 
-	logger.Infof("Received response: \"%s\"", resp)
+	if err != nil {
+		logFriendlyError(logger, err)
+		return err
+	}
+
+	expectedResp := []redis.XStream{
+		{
+			Stream: randomKey,
+			Messages: []redis.XMessage{
+				{
+					ID:     "0-2",
+					Values: map[string]interface{}{"bar": "baz"},
+				},
+			},
+		},
+	}
+
+	if !reflect.DeepEqual(resp, expectedResp) {
+		logger.Infof("Received response: %#v", resp)
+		return fmt.Errorf("Expected %#v, got %#v", expectedResp, resp)
+	} else {
+		logger.Successf("Received response: %#v", resp)
+	}
+
+	resp, err = client.XRead(&redis.XReadArgs{
+		Block:   1000 * time.Millisecond,
+		Streams: []string{randomKey, "0-2"},
+	}).Result()
+
+	if err.Error() != "redis: nil" {
+		logFriendlyError(logger, err)
+		return err
+	}
+
+	if resp != nil {
+		logger.Infof("this is a test")
+		return fmt.Errorf("Expected %#v, got %#v", []redis.XStream{}, resp)
+	} else {
+		logger.Successf("Received response: %#v", resp)
+	}
+
 	return nil
 }
