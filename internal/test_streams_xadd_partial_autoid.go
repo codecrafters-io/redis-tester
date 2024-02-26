f8f2fed85e39365487fd7cb996599dfcaf839d22
libmartinito <lib.martinito@pm.me>
2024-02-26 20:31:18 +0800

    CC-986 updated tester function

--- internal/test_streams_xadd_partial_autoid.go
+++ internal/test_streams_xadd_partial_autoid.go
@@ -1,11 +1,9 @@
 package internal
 
 import (
-	"fmt"
 	"math/rand"
 
 	testerutils "github.com/codecrafters-io/tester-utils"
-	"github.com/go-redis/redis"
 )
 
 func testStreamsXaddPartialAutoid(stageHarness *testerutils.StageHarness) error {
@@ -33,61 +31,14 @@ func testStreamsXaddPartialAutoid(stageHarness *testerutils.StageHarness) error 
 
 	randomKey := strings[rand.Intn(10)]
 
-	logger.Infof("$ redis-cli xadd %s 0-* foo bar", randomKey)
-
-	resp, err := client.XAdd(&redis.XAddArgs{
-		Stream: randomKey,
-		ID:     "0-*",
-		Values: map[string]interface{}{
-			"foo": "bar",
-		},
-	}).Result()
-
-	if err != nil {
-		logFriendlyError(logger, err)
-		return err
+	tests := []XADDTest{
+		{streamKey: randomKey, id: "0-*", values: map[string]interface{}{"foo": "bar"}, expectedResponse: "0-1"},
+		{streamKey: randomKey, id: "1-*", values: map[string]interface{}{"foo": "bar"}, expectedResponse: "1-0"},
+		{streamKey: randomKey, id: "1-*", values: map[string]interface{}{"bar": "baz"}, expectedResponse: "1-1"},
 	}
 
-	if resp != "0-1" {
-		return fmt.Errorf("Expected \"0-1\", got %#v", resp)
-	}
-
-	logger.Infof("$ redis-cli xadd %s 1-* foo bar", randomKey)
-
-	resp, err = client.XAdd(&redis.XAddArgs{
-		Stream: randomKey,
-		ID:     "1-*",
-		Values: map[string]interface{}{
-			"foo": "bar",
-		},
-	}).Result()
-
-	if err != nil {
-		logFriendlyError(logger, err)
-		return err
-	}
-
-	if resp != "1-0" {
-		return fmt.Errorf("Expected \"1-0\", got %#v", resp)
-	}
-
-	logger.Infof("$ redis-cli xadd %s 1-* bar baz", randomKey)
-
-	resp, err = client.XAdd(&redis.XAddArgs{
-		Stream: randomKey,
-		ID:     "1-*",
-		Values: map[string]interface{}{
-			"bar": "baz",
-		},
-	}).Result()
-
-	if err != nil {
-		logFriendlyError(logger, err)
-		return err
-	}
-
-	if resp != "1-1" {
-		return fmt.Errorf("Expected \"1-1\", got %#v", resp)
+	for _, test := range tests {
+		testXadd(client, logger, test)
 	}
 
 	return nil
