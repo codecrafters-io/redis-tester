93cac46ad3003f206d3d6586c381bd38a45cb3a5
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-02-29 10:19:05 +0000

    Refactor InspectableByteString struct in internal/inspectable_byte_string package

--- internal/inspectable_byte_string/inspectable_byte_string.go
+++ internal/inspectable_byte_string/inspectable_byte_string.go
@@ -15,28 +15,15 @@ func NewInspectableByteString(bytes []byte) InspectableByteString {
 	return InspectableByteString{bytes: bytes}
 }
 
-// GetOffsetInFormattedString returns a string that represents the byteOffset in the formatted string
-//
-// For example:
-//   - If the string is "+OK\r\n"
-//   - And byteOffset is 4 (i.e. \n, the 5th byte)
-//   - The return value will be 6 (i.e. the 6th character in the formatted string)
-func (s InspectableByteString) GetOffsetInFormattedString(byteOffset int) int {
-	if s.truncationStartIndex != 0 {
-		byteOffset = byteOffset - s.truncationStartIndex
-	}
-
-	formattedBytesBefore := fmt.Sprintf("%q", string(s.bytes[:byteOffset]))
-	return len(formattedBytesBefore) - 1
-}
-
 // FormatWithHighlightedOffset returns a string that represents the bytes with the byteOffset highlighted
 //
 // For example, if called with highlightOffset 4, highlightText "error" and formattedString "Received: ", the return value will be:
 //
 // ```
 // Received: "+OK\r\n"
+//
 //	^ error
+//
 // ```
 func (s InspectableByteString) FormatWithHighlightedOffset(highlightOffset int, highlightText string, formattedStringPrefix string) string {
 	s = s.TruncateAroundOffset(highlightOffset)
@@ -66,4 +53,19 @@ func (s InspectableByteString) TruncateAroundOffset(offset int) InspectableByteS
 		bytes:                s.bytes[start:end],
 		truncationStartIndex: start,
 	}
+}
+
+// GetOffsetInFormattedString returns a string that represents the byteOffset in the formatted string
+//
+// For example:
+//   - If the string is "+OK\r\n"
+//   - And byteOffset is 4 (i.e. \n, the 5th byte)
+//   - The return value will be 6 (i.e. the 6th character in the formatted string)
+func (s InspectableByteString) GetOffsetInFormattedString(byteOffset int) int {
+	if s.truncationStartIndex != 0 {
+		byteOffset = byteOffset - s.truncationStartIndex
+	}
+
+	formattedBytesBefore := fmt.Sprintf("%q", string(s.bytes[:byteOffset]))
+	return len(formattedBytesBefore) - 1
 }
