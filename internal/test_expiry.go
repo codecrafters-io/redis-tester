69bccc9f6f0624159e60fe45c1188c7b32be6d58
Ryan Gang <ryan-gg@outlook.com>
2024-03-11 15:56:48 +0530

    refactor: Use updated NewRedisExecutable and NewInstrumentedRespCient.
    Updated the import paths for the instrumented_resp_connection package and redis_executable package in test cases to reflect the restructuring of the project. This change was done to ensure proper functionality and alignment with the new project structure.

--- internal/test_expiry.go
+++ internal/test_expiry.go
@@ -3,7 +3,8 @@ package internal
 import (
 	"time"
 
-	"github.com/codecrafters-io/redis-tester/internal/instrumented_resp_client"
+	"github.com/codecrafters-io/redis-tester/internal/instrumented_resp_connection"
+	"github.com/codecrafters-io/redis-tester/internal/redis_executable"
 	"github.com/codecrafters-io/redis-tester/internal/resp_assertions"
 	"github.com/codecrafters-io/redis-tester/internal/test_cases"
 	"github.com/codecrafters-io/tester-utils/random"
@@ -12,14 +13,14 @@ import (
 
 // Tests Expiry
 func testExpiry(stageHarness *test_case_harness.TestCaseHarness) error {
-	b := NewRedisBinary(stageHarness)
-	if err := b.Run(); err != nil {
+	b := redis_executable.NewRedisExecutable(stageHarness)
+	if err := b.Run([]string{}); err != nil {
 		return err
 	}
 
 	logger := stageHarness.Logger
 
-	client, err := instrumented_resp_client.NewInstrumentedRespClient(stageHarness, "localhost:6379", "")
+	client, err := instrumented_resp_connection.NewInstrumentedRespClient(stageHarness, "localhost:6379", "")
 	if err != nil {
 		logFriendlyError(logger, err)
 		return err
