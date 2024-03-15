f0fa419ba8003d2fb9c6f5ce6122b46ce94b1d94
Ryan Gang <ryan-gg@outlook.com>
2024-03-15 15:03:03 +0530

    refactor: refactor base stages.
    Use new RESP parser and assertions.

--- internal/test_repl_bind.go
+++ internal/test_repl_bind.go
@@ -1,18 +1,21 @@
 package internal
 
 import (
+	"strconv"
+
 	"github.com/codecrafters-io/redis-tester/internal/redis_executable"
 	"github.com/codecrafters-io/redis-tester/internal/test_cases"
 	testerutils_random "github.com/codecrafters-io/tester-utils/random"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
-	"strconv"
 )
 
 func testReplBindToCustomPort(stageHarness *test_case_harness.TestCaseHarness) error {
 	port := testerutils_random.RandomInt(6380, 6390)
 
 	b := redis_executable.NewRedisExecutable(stageHarness)
-	if err := b.Run([]string{"--port", strconv.Itoa(port)}...); err != nil {
+	if err := b.Run([]string{
+		"--port", strconv.Itoa(port),
+	}...); err != nil {
 		return err
 	}
 
