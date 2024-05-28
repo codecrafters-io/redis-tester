8dd3722f0d0dd6bc0da66d0c69f751f62cff59c5
Ryan Gang <ryan-gg@outlook.com>
2024-05-28 22:02:19 +0530

    CC-1226: Use updated RunCLI interface from tester-utils

--- internal/cli.go
+++ internal/cli.go
@@ -2,18 +2,9 @@
 package internal
 
 import (
-	"fmt"
-
 	testerutils "github.com/codecrafters-io/tester-utils"
 )
 
 func RunCLI(env map[string]string) int {
-	tester, err := testerutils.NewTester(env, testerDefinition)
-
-	if err != nil {
-		fmt.Printf("%s", err)
-		return 1
-	}
-
-	return tester.RunCLI()
+	return testerutils.RunCLI(env, testerDefinition)
 }
