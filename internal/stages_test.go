0bd8bac118e7d1d8a6a78006c3ada0e48a21ea6a
Ryan Gang <ryan-gg@outlook.com>
2024-06-13 14:53:20 +0530

    tests: add fixtures for transaction stages

--- internal/stages_test.go
+++ internal/stages_test.go
@@ -89,6 +89,13 @@ func TestStages(t *testing.T) {
 			StdoutFixturePath:   "./test_helpers/fixtures/streams/pass",
 			NormalizeOutputFunc: normalizeTesterOutput,
 		},
+		"transactions_pass": {
+			UntilStageSlug:      "jf8",
+			CodePath:            "./test_helpers/pass_all",
+			ExpectedExitCode:    0,
+			StdoutFixturePath:   "./test_helpers/fixtures/transactions/pass",
+			NormalizeOutputFunc: normalizeTesterOutput,
+		},
 	}
 
 	tester_utils_testing.TestTesterOutput(t, testerDefinition, testCases)
