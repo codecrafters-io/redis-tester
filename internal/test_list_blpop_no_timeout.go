d94b3da5ad160f6032b5b9c751c08e98e71f3bcc
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-07-01 21:21:47 -0700

    Refactor testListBlpopNoTimeout to use new test case structure for clarity.

--- internal/test_list_blpop_no_timeout.go
+++ internal/test_list_blpop_no_timeout.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"github.com/codecrafters-io/redis-tester/internal/redis_executable"
+	resp_connection "github.com/codecrafters-io/redis-tester/internal/resp/connection"
 	"github.com/codecrafters-io/redis-tester/internal/resp_assertions"
 	"github.com/codecrafters-io/redis-tester/internal/test_cases"
 	testerutils_random "github.com/codecrafters-io/tester-utils/random"
@@ -24,42 +25,42 @@ func testListBlpopNoTimeout(stageHarness *test_case_harness.TestCaseHarness) err
 		defer c.Close()
 	}
 
+	sendingClient := clients[0]
+	firstBlockingClient := clients[1]
+	secondBlockingClient := clients[2]
+	blockingClients := []*resp_connection.RespConnection{firstBlockingClient, secondBlockingClient}
+
 	listKey := testerutils_random.RandomWord()
 	pushValue := testerutils_random.RandomWord()
 
-	blPopAssertion := resp_assertions.NewOrderedStringArrayAssertion([]string{listKey, pushValue})
-
-	blockingTestCase := test_cases.BlockingCommandTestCase{
-		BlockingClientsTestCases: []test_cases.ClientTestCase{
-			{
-				Client: clients[0],
-				SendCommandTestCase: &test_cases.SendCommandTestCase{
-					Command:   "BLPOP",
-					Args:      []string{listKey, "0"},
-					Assertion: blPopAssertion,
-				},
-				ExpectResult: true,
-			},
-			{
-				Client: clients[1],
-				SendCommandTestCase: &test_cases.SendCommandTestCase{
-					Command:   "BLPOP",
-					Args:      []string{listKey, "0"},
-					Assertion: blPopAssertion,
-				},
-				ExpectResult: false,
-			},
-		},
-		UnblockingClientTestCase: &test_cases.ClientTestCase{
-			Client: clients[2],
-			SendCommandTestCase: &test_cases.SendCommandTestCase{
-				Command:   "RPUSH",
-				Args:      []string{listKey, pushValue},
-				Assertion: resp_assertions.NewIntegerAssertion(1),
-			},
-			ExpectResult: true,
-		},
+	// We only send commands here, not expecting responses yet
+	for _, client := range blockingClients {
+		if err := client.SendCommand("BLPOP", listKey, "0"); err != nil {
+			return err
+		}
 	}
 
-	return blockingTestCase.Run(logger)
+	sendCommandTestCase := test_cases.SendCommandTestCase{
+		Command:   "RPUSH",
+		Args:      []string{listKey, pushValue},
+		Assertion: resp_assertions.NewIntegerAssertion(1),
+	}
+
+	if err := sendCommandTestCase.Run(sendingClient, logger); err != nil {
+		return err
+	}
+
+	firstBlockingClientTestCase := test_cases.ReceiveValueTestCase{
+		Assertion: resp_assertions.NewOrderedStringArrayAssertion([]string{listKey, pushValue}),
+	}
+
+	if err := firstBlockingClientTestCase.Run(firstBlockingClient, logger); err != nil {
+		return err
+	}
+
+	secondBlockingClientTestCase := test_cases.NoUnreadDataTestCase{}
+
+	if err := secondBlockingClientTestCase.Run(secondBlockingClient, logger); err != nil {
+		return err
+	}
 }
