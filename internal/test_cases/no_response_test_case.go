85351696cf2f6b953458c6d5429d2891dd4e1407
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2025-07-15 13:22:22 +0545

    fix: move logs to noresponsetestcase

--- internal/test_cases/no_response_test_case.go
+++ internal/test_cases/no_response_test_case.go
@@ -9,6 +9,7 @@ import (
 type NoResponseTestCase struct{}
 
 func (n *NoResponseTestCase) Run(client *instrumented_resp_connection.InstrumentedRespConnection) error {
+	client.GetLogger().Infof("Expecting no response")
 	client.ReadIntoBuffer()
 	if client.UnreadBuffer.Len() > 0 {
 		return fmt.Errorf("%s received unexpected response: %q", client.GetIdentifier(), client.UnreadBuffer.String())
