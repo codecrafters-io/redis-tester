c28e99a89a5f09f6a096362eeb93ce1c166815c8
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2026-03-13 09:59:13 +0545

    Move remaining stages to the clients spawner construct and record fixtures

--- internal/test_list_rpush3.go
+++ internal/test_list_rpush3.go
@@ -1,7 +1,6 @@
 package internal
 
 import (
-	"github.com/codecrafters-io/redis-tester/internal/instrumented_resp_connection"
 	"github.com/codecrafters-io/redis-tester/internal/redis_executable"
 	"github.com/codecrafters-io/redis-tester/internal/resp_assertions"
 	"github.com/codecrafters-io/redis-tester/internal/test_cases"
@@ -16,12 +15,15 @@ func testListRpush3(stageHarness *test_case_harness.TestCaseHarness) error {
 	}
 
 	logger := stageHarness.Logger
-	client, err := instrumented_resp_connection.NewFromAddr(logger, "localhost:6379", "client")
+
+	clientsSpawner := ClientsSpawner{
+		Addr:         "localhost:6379",
+		StageHarness: stageHarness,
+	}
+	client, err := clientsSpawner.SpawnClientWithPrefix("client")
 	if err != nil {
-		logFriendlyError(logger, err)
 		return err
 	}
-	defer client.Close()
 
 	listKey := testerutils_random.RandomWord()
 
--- internal/test_repl_master_replconf.go
+++ internal/test_repl_master_replconf.go
@@ -1,7 +1,6 @@
 package internal
 
 import (
-	"github.com/codecrafters-io/redis-tester/internal/instrumented_resp_connection"
 	"github.com/codecrafters-io/redis-tester/internal/redis_executable"
 	"github.com/codecrafters-io/redis-tester/internal/test_cases"
 
@@ -16,12 +15,14 @@ func testReplMasterReplconf(stageHarness *test_case_harness.TestCaseHarness) err
 
 	logger := stageHarness.Logger
 
-	client, err := instrumented_resp_connection.NewFromAddr(logger, "localhost:6379", "client")
+	clientsSpawner := ClientsSpawner{
+		Addr:         "localhost:6379",
+		StageHarness: stageHarness,
+	}
+	client, err := clientsSpawner.SpawnClientWithPrefix("client")
 	if err != nil {
-		logFriendlyError(logger, err)
 		return err
 	}
-	defer client.Close()
 
 	sendHandshakeTestCase := test_cases.SendReplicationHandshakeTestCase{}
 
