4e3ade5ed80353cf13522c8a3ec8e558b134725f
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2026-04-01 11:10:19 +0545

    Add hints for both expected and received data type

--- internal/resp_assertions/data_type_assertion.go
+++ internal/resp_assertions/data_type_assertion.go
@@ -11,22 +11,46 @@ type DataTypeAssertion struct {
 }
 
 func (a DataTypeAssertion) Run(value resp_value.Value) error {
-	dataTypeHint := ""
+	if value.Type == a.ExpectedType {
+		return nil
+	}
 
-	switch a.ExpectedType {
+	expectedDataTypeHint := a.getDataTypeHint(a.ExpectedType)
+	receivedDataTypeHint := a.getDataTypeHint(value.Type)
+
+	// Spacing
+	if expectedDataTypeHint != "" {
+		expectedDataTypeHint = fmt.Sprintf(" (%s)", expectedDataTypeHint)
+	}
+
+	if receivedDataTypeHint != "" {
+		receivedDataTypeHint = fmt.Sprintf(" (%s)", receivedDataTypeHint)
+	}
+
+	if value.Type != a.ExpectedType {
+		return fmt.Errorf(
+			"Expected %s%s, found %s%s",
+			a.ExpectedType, expectedDataTypeHint,
+			value.Type, receivedDataTypeHint,
+		)
+	}
+	return nil
+}
+
+func (a DataTypeAssertion) getDataTypeHint(dataType string) string {
+	dataTypeHint := ""
+	switch dataType {
 	case resp_value.NIL:
 		dataTypeHint = "$-1\r\n"
 	case resp_value.NIL_ARRAY:
 		dataTypeHint = "*-1\r\n"
+	default:
+		dataType = ""
 	}
 
-	// Spacing
 	if dataTypeHint != "" {
-		dataTypeHint = " " + dataTypeHint
+		dataTypeHint = FormatWithoutQuotes(dataTypeHint)
 	}
 
-	if value.Type != a.ExpectedType {
-		return fmt.Errorf("Expected %s%s, found %s", a.ExpectedType, dataTypeHint, value.Type)
-	}
-	return nil
+	return dataTypeHint
 }
