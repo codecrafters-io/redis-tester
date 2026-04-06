5a830a684f9d23dc0a67a71364e989686ae0ca96
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2026-04-06 10:01:50 +0545

    Remove debug log

--- internal/test_bind.go
+++ internal/test_bind.go
@@ -14,8 +14,6 @@ func testBindToPort(stageHarness *test_case_harness.TestCaseHarness) error {
 
 	logger := stageHarness.Logger
 
-	logger.Infof("Test log for testing regenerate_fixtures")
-
 	bindTestCase := test_cases.BindTestCase{
 		Port:    6379,
 		Retries: 15,
